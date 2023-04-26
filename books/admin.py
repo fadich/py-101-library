bd09a74ece2503d4f145c60d314e3d64c655ea63
gitedcode <116921747+gitedcode@users.noreply.github.com>
2023-04-26 21:28:20 +0300

    Adding Books, Genre, Publisher

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from books import models
 
 # Register your models here.
+
+
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
+
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ["name", "country"]
+    list_filter = ["country"]
+    search_fields = ["name", "country__name"]
+
+
+admin.site.register(models.Country, CountryAdmin)
+admin.site.register(models.Author, AuthorAdmin)
+admin.site.register(models.Genre, GenreAdmin)
+admin.site.register(models.Publisher, PublisherAdmin)
+admin.site.register(models.Book)
--- books/models.py
+++ books/models.py
@@ -5,9 +5,52 @@ class Country(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name_plural = "Countries"
+
+    def __str__(self):
+        return self.name
+
 
 class Author(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(unique=True, max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Publisher(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = [
+            ["name", "country"]
+        ]
+    def __str__(self):
+        return f"{self.name}, {self.country}"
+
+
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=20, unique=True)
+    description = models.TextField()
+    year_publishing = models.SmallIntegerField()
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return f"{self.name}, {self.authors.first()}, {self.year_publishing}"
