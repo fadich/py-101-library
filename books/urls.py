6f1211c69759ba5fc2fbddb73b6c7034186705ec
Bohdan Chornous <bodyablack110799@gmail.com>
2023-05-03 20:13:56 +0100

    Added-login page

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from .views import home, BookListView, BookView
+from .views import home, BookListView, BookView, user_login
 
 urlpatterns = [
-
     path('', BookListView.as_view()),
-    path('<int:pk>/', BookView.as_view())
+    path('<int:pk>/', BookView.as_view()),
+    path('login/', user_login, name='user_login')
 ]
--- books/views.py
+++ books/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
 from .models import Book
 from django.views.generic import TemplateView, DetailView
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+
+
+def user_login(request):
+    if request.method == "POST":
+        user_name = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=user_name, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/books/')
+        else:
+            return render(request, 'user_login.html', context={
+                'error': 'invalid credentials'
+            })
+    else:
+        return render(request, 'user_login.html')
 
 
 def home(request):
     books = Book.objects.all()
-    return render(request,'home.html',{'books':books})
+    return render(request, 'home.html', {'books': books})
+
 
 class BookListView(TemplateView):
     template_name = 'home.html'
@@ -15,6 +34,7 @@ class BookListView(TemplateView):
         context['books'] = Book.objects.all()
 
         return context
+
 
 class BookView(DetailView):
     template_name = 'book_details.html'
